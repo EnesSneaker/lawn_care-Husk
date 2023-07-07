df98c0c8bb1d2b4a8408ed27ab660a82becaca21
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2023-07-07 21:20:48 +0200

    for deploy

--- src/app/header-footer-layout/footer.tsx
+++ src/app/header-footer-layout/footer.tsx
@@ -20,7 +20,7 @@ export default function DefaultFooter() {
                 <span className="text-sm text-black-500 sm:text-center">
                     © 2023{" "}
                     <a href="https://flowbite.com/" className="hover:underline">
-                        Husko's Lawn Care™
+                        Husko`s Lawn Care™
                     </a>
                     . All Rights Reserved.
                 </span>
