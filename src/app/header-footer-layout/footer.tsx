53572915da755cbbaec6a42fb012150af707beba
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2023-07-07 21:25:08 +0200

    for deploying

--- src/app/header-footer-layout/footer.tsx
+++ src/app/header-footer-layout/footer.tsx
@@ -1,26 +1,15 @@
 "use client";
 
 import React from "react";
-import { Footer } from "flowbite-react";
 
 export default function DefaultFooter() {
     return (
-        // <Footer container classNameName="h-20 bg-green-500 mt-8">
-        //     <Footer.Copyright by="Lawn Care™" href="#" year={2023} />
-        //     <Footer.LinkGroup>
-        //         <Footer.Link href="#">About</Footer.Link>
-        //         <Footer.Link href="#">Privacy Policy</Footer.Link>
-        //         <Footer.Link href="#">Licensing</Footer.Link>
-        //         <Footer.Link href="#">Contact</Footer.Link>
-        //     </Footer.LinkGroup>
-        // </Footer>
-
         <footer className="border-2 border-green-500 rounded-lg shadow m-4 ">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-black-500 sm:text-center">
                     © 2023{" "}
                     <a href="https://flowbite.com/" className="hover:underline">
-                        Husko`s Lawn Care™
+                        Huskos Lawn Care™
                     </a>
                     . All Rights Reserved.
                 </span>
--- src/app/header-footer-layout/header.tsx
+++ src/app/header-footer-layout/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <a href="/" className="flex items-center">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            Husko's Lawn Care
+                            Huskos Lawn Care
                         </span>
                     </a>
                     <div className="flex items-center">
