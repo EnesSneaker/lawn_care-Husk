e7cadf24d2ce22c171208509ea50b95d2e78e177
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2023-07-29 12:32:34 +0200

    Meta Header

--- src/app/images.tsx
+++
@@ -1,8 +0,0 @@
-import image1 from "./assets/husko-test1.jpg";
-import image2 from "./assets/husko-test2.jpg";
-import image3 from "./assets/husko-test3.jpg";
-import image4 from "./assets/husko-test4.jpg";
-import image5 from "./assets/husko-test5.jpg";
-import image6 from "./assets/husko-test6.jpg";
-
-export default [image1, image2, image3, image4, image5, image6];
