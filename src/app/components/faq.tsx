9d3bb086bbd255d51a0a43a8c67ab486e060feab
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2023-08-08 20:13:34 +0200

    fix faq phone screen

--- src/app/components/faq.tsx
+++ src/app/components/faq.tsx
@@ -109,7 +109,7 @@ export default function Faq() {
     ];
 
     return (
-        <div className="py-10 px-4 mx-auto sm:px-6 md:px-6 lg:px-8 max-w-7xl sm:py-16 lg:py-24">
+        <div className="px-4 mx-auto sm:px-6 md:px-6 lg:px-8 max-w-7xl sm:py-16 lg:py-24">
             <div className="max-w-2xl mx-auto text-center">
                 <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
                     Frequently Asked Questions
