b3158e4a30545421bd5eb3dbae1446e45efa2d27
Enes Ejubovic <enes.ejubovic.bus@gmail.com>
2023-10-19 20:41:14 +0200

    change subject text email

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -8,7 +8,7 @@ const sendEmail = async ({ subject, to, text, email }: any,) => { // subject = n
     sgMail.setApiKey(SENDGRID_API_KEY!);
     
     
-    subject = `${subject} need helps`
+    subject = `${subject} needs help`
     text = `Email: ${email} \n 
             Message: ${text}`;
 
